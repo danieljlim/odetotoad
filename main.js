8847ba28428b65fa395dcc128aec44a5251b7dbf
Daniel Lim <d.lim@live.co.uk>
2023-12-17 10:30:08 +0000

    Scaled canvas to fit screen, refactored constants, added particle effects when collecting bread.

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
+// TODO:
+// [X] resize for mobile
+// [X] scale to fit screen
+// [X] refactor constants
+// [X] add particle effects when collecting bread
+// [ ] refactor item collection
+// [ ] add toad 
+
+const SIZES = {
+    WIDTH: 1024,
+    HEIGHT: 1024
+}
+
+const GRAVITY = 450;
+const JUMP_VELOCITY = -300;
+
+
 var config = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 1024,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: SIZES.WIDTH,
+        height: SIZES.HEIGHT
+    },
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y: GRAVITY }
         }
     },
     scene: {
@@ -24,15 +45,16 @@ function preload() {
     this.load.image('background', 'assets/background2.png');
     this.load.spritesheet('player', 'assets/yoko.png', { frameWidth: 88, frameHeight: 128 });
     this.load.spritesheet('bread', 'assets/bread.png', { frameWidth: 128, frameHeight: 88 });
+    this.load.image('breadcrumb', 'assets/breadcrumb.png');
 }
 
 function create() {
     // Create game objects here
     // Add the background image as a tile sprite
-    this.background = this.add.tileSprite(512, 512, 1792, 1024, 'background');
+    this.background = this.add.tileSprite(SIZES.WIDTH/2, SIZES.HEIGHT/2, 1792, 1024, 'background');
 
     // Add the player sprite
-    this.player = this.physics.add.sprite(100, 450, 'player');
+    this.player = this.physics.add.sprite(100, SIZES.HEIGHT/2, 'player');
 
     // Set player properties
     this.player.setBounce(0.2);
@@ -53,10 +75,13 @@ function create() {
         callbackScope: this,
         loop: true
     });
+
+    // Initialize the particles property
+    this.particles = this.add.particles('breadcrumb');
     
     // Function to make the player jump
     var jump = () => {
-        this.player.setVelocityY(-330);
+        this.player.setVelocityY(JUMP_VELOCITY);
     };
 
     // Make the player jump when the spacebar is pressed
@@ -81,9 +106,33 @@ function update() {
 function collectBread(player, bread) {
     bread.disableBody(true, true);
 
+    makePlayerHappy(player, this)
+
+    // Increase and display the score
+    score += 1;
+    scoreText.setText('Score: ' + score);
+    
+    // Create a new particle emitter
+    var emitter = this.add.particles(bread.x, bread.y, 'breadcrumb', {
+        speed: 50,
+        scale: { start: 0.3, end: 0.6 },
+        angle: { min: 0, max: 360 },
+        blendMode: 'NORMAL'
+    });
+
+    // Stop emitting particles after a short time
+    this.time.addEvent({
+        delay: 150,
+        callback: function() {
+            emitter.stop();
+        },
+        callbackScope: this
+    });
+}
+
+function makePlayerHappy(player, scene) {
     // Change the player's sprite to the second frame
     player.setFrame(1);
-
 
     // If there's a previous timer, cancel it
     if (player.resetFrameTimer) {
@@ -91,11 +140,7 @@ function collectBread(player, bread) {
     }
 
     // Change the player's sprite back to the first frame after a delay
-    player.resetFrameTimer = this.time.delayedCall(500, function() {
+    player.resetFrameTimer = scene.time.delayedCall(500, function() {
         player.setFrame(0);
-    }, [], this);   
-
-    // Increase and display the score
-    score += 1;
-    scoreText.setText('Score: ' + score);
+    }, [], scene);
 }
